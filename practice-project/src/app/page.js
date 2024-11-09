0a0789de1cc49ede6d6e161d193a2f5496cc7864
Vaibhav <vaibhavjam86@gmail.com>
2024-11-09 22:33:28 +0530

    created user compo and reuse that-(reusable compo)

--- practice-project/src/app/page.js
+++ practice-project/src/app/page.js
@@ -1,10 +1,19 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Learning NEXT.JS</h1>
+      <User />
+      {/* <User />  resusable compo*/}
     </div>
   );
 }
+
+const User = () => {
+  return (
+    <div>
+      <h1>User name is Vaibhav</h1>
+    </div>
+  );
+};
