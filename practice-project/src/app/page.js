680bfd2ed5c1e556cfcddaa9413ddd8baa9d47e5
Vaibhav <vaibhavjam86@gmail.com>
2024-11-09 22:43:55 +0530

    props(passing data)

--- practice-project/src/app/page.js
+++ practice-project/src/app/page.js
@@ -4,16 +4,18 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Learning NEXT.JS</h1>
-      <User />
+      <User name="vaibhav" surname="jamdhade" />
+      {/* <User name="satish" surname="shejwal" /> */}
       {/* <User />  resusable compo*/}
     </div>
   );
 }
 
-const User = () => {
+const User = (props) => {
   return (
     <div>
-      <h1>User name is Vaibhav</h1>
+      <h1>{`User name is ${props.name} ${props.surname} `}</h1>
+      {/* <p>User name is {props.name}</p> */}
     </div>
   );
 };
