b214cfb9bf4714e5a1b1dc27d132103df24b973d
Vaibhav <vaibhavjam86@gmail.com>
2024-11-11 11:14:05 +0530

    Learn functions,events and state also tried running compo inside compo

--- practice-project/src/app/page.js
+++ practice-project/src/app/page.js
@@ -1,21 +1,26 @@
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [name, setName] = useState("Vaibhav");
+  const greet = () => {
+    // alert("Hello,Welcome to the world of Next.js");
+    setName("Satish");
+  };
+
+  const InnerCompo = () => {
+    return <div>Inner Component</div>;
+  };
+
   return (
     <div className={styles.page}>
       <h1>Learning NEXT.JS</h1>
-      <User name="vaibhav" surname="jamdhade" />
-      {/* <User name="satish" surname="shejwal" /> */}
-      {/* <User />  resusable compo*/}
+      <h2>MySelf {name}</h2>
+      <h2>Functions,Events and States</h2>
+      <button onClick={greet}>Click Me</button>
+      {/* <InnerCompo /> */}
+      {InnerCompo()}
     </div>
   );
 }
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{`User name is ${props.name} ${props.surname} `}</h1>
-      {/* <p>User name is {props.name}</p> */}
-    </div>
-  );
-};
