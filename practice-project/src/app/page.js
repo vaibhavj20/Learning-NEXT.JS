5711b065dd4939692960347690acea213099ffee
Vaibhav <vaibhavjam86@gmail.com>
2024-11-12 22:37:00 +0530

    removed module css

--- practice-project/src/app/page.js
+++ practice-project/src/app/page.js
@@ -1,26 +1,13 @@
 "use client";
-import { useState } from "react";
-import styles from "./page.module.css";
+import "./page.css";
+
+// import styles from "./page.module.css";
 
 export default function Home() {
-  const [name, setName] = useState("Vaibhav");
-  const greet = () => {
-    // alert("Hello,Welcome to the world of Next.js");
-    setName("Satish");
-  };
-
-  const InnerCompo = () => {
-    return <div>Inner Component</div>;
-  };
-
   return (
-    <div className={styles.page}>
-      <h1>Learning NEXT.JS</h1>
-      <h2>MySelf {name}</h2>
-      <h2>Functions,Events and States</h2>
-      <button onClick={greet}>Click Me</button>
-      {/* <InnerCompo /> */}
-      {InnerCompo()}
+    <div className="para">
+      <h1 className="heading-clr">Learning NEXT.JS</h1>
+      <h2>Basic Routing</h2>
     </div>
   );
 }
