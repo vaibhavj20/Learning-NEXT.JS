7bb15d4cf801ff3c058bcbf0ba69ab7b09a8bc01
Vaibhav <vaibhavjam86@gmail.com>
2024-11-24 11:21:24 +0530

    updated

--- practice-project/src/app/login/page.js
+++ practice-project/src/app/login/page.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>This is login page</div>;
+  return (
+    <div>
+      <h1>This is login page</h1>
+      <Link href="/">Go to homepage</Link>
+    </div>
+  );
 };
 
 export default LoginPage;
