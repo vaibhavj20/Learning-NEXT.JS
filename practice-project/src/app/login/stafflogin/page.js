5246d75ff8c0c3154b5465136b87f1d7780b9960
Vaibhav <vaibhavjam86@gmail.com>
2024-11-30 22:12:48 +0530

    subpages created

---
+++ practice-project/src/app/login/stafflogin/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StaffLogin = () => {
+  return <div>StaffLogin page is here</div>;
+};
+
+export default StaffLogin;
---
+++ practice-project/src/app/login/studentlogin/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentLogin = () => {
+  return <div>StudentLogin page is here</div>;
+};
+
+export default StudentLogin;
