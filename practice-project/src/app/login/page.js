6bcd789a2b8182093cd2ebc137c38eb5221f36f9
Vaibhav <vaibhavjam86@gmail.com>
2024-11-30 22:21:19 +0530

    added navigation btn for student login page

--- practice-project/src/app/login/page.js
+++ practice-project/src/app/login/page.js
@@ -1,17 +1,24 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const navigate = useRouter();
   return (
     <div>
       <h1>This is login page</h1>
       <Link href="/">Go to homepage</Link>
       <br />
       <br />
-      <Link href="login/studentlogin">Student Login</Link>
+      {/* <Link href="login/studentlogin">Student Login</Link> */}
       <br />
       <br />
-      <Link href="login/stafflogin">Staff Login</Link>
+      {/* <Link href="login/stafflogin">Staff Login</Link> */}
+      <br />
+      <button onClick={() => navigate.push("/login/studentlogin")}>
+        Go to student login page
+      </button>
     </div>
   );
 };
