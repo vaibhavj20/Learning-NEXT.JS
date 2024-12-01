6c58b961540d070551b9b8cae4586726aa65fdeb
Vaibhav <vaibhavjam86@gmail.com>
2024-12-01 23:07:37 +0530

    added common layout for about pages

---
+++ practice-project/src/app/about/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Aboutlayout = ({ children }) => {
+  return (
+    <div>
+      Common layout for about screen
+      {children}
+    </div>
+  );
+};
+
+export default Aboutlayout;
