aaadbc91f04fbcdf18ecec8f368b60c716598a6b
Vaibhav <vaibhavjam86@gmail.com>
2024-12-02 11:30:42 +0530

    added conditional layout

--- practice-project/src/app/login/layout.js
+++ practice-project/src/app/login/layout.js
@@ -1,24 +1,30 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import "./login.css";
+import { usePathname } from "next/navigation";
 
 const LoginLayout = ({ children }) => {
+  const pathName = usePathname();
+  console.log(pathName);
   return (
     <div>
-      <ul className="login-menu">
-        <li>
-          <h4>Login Navbar</h4>
-        </li>
-        <li>
-          <Link href="/login">Login Main</Link>
-        </li>
-        <li>
-          <Link href="/login/studentlogin">Student Login</Link>
-        </li>
-        <li>
-          <Link href="/login/stafflogin">Staff Login</Link>
-        </li>
-      </ul>
+      {pathName !== "/login/stafflogin" ? (
+        <ul className="login-menu">
+          <li>
+            <h4>Login Navbar</h4>
+          </li>
+          <li>
+            <Link href="/login">Login Main</Link>
+          </li>
+          <li>
+            <Link href="/login/studentlogin">Student Login</Link>
+          </li>
+          <li>
+            <Link href="/login/stafflogin">Staff Login</Link>
+          </li>
+        </ul>
+      ) : null}
       {children}
     </div>
   );
